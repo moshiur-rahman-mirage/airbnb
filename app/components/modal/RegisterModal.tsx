cb97b4fe8838817128d430c4748713386e54b9a1
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2024-04-15 19:57:36 +0600

    worked on modal and input

--- app/components/modal/RegisterModal.tsx
+++ app/components/modal/RegisterModal.tsx
@@ -6,6 +6,8 @@ import {Field, FieldValues,SubmitHandler,useForm} from "react-hook-form"
 import useRegisterModal from "@/app/hooks/useRegisterModal"
 import { useState } from "react"
 import Modal from "./Modal"
+import Heading from "../Heading"
+import Inputs from "../inputs/Inputs"
 
 const RegisterModal = () => {
     const registerModal=useRegisterModal()
@@ -38,6 +40,20 @@ const RegisterModal = () => {
         })
     }
 
+    const bodyContent=(
+        <div className="flex flex-col gap-4">
+            <Heading title="Welcome to AirBnb" subtitle="Create an Account"/>
+            <Inputs 
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    )
+
   return (
     <Modal
         disabled={isLoading}
@@ -46,6 +62,7 @@ const RegisterModal = () => {
         actionLabel="Continue"
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
+        body={bodyContent}
     />
   )
 }
